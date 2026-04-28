0edc1265725e4cd3dc4319c58dcb8aad4b0eadb2
Luiz-Ponzini <126832392+Luiz-Ponzini@users.noreply.github.com>
2026-04-28 02:21:25 -0300

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -11,7 +11,7 @@ st.set_page_config(layout="wide")
 # ---------------------------
 # DADOS (SUPABASE)
 # ---------------------------
-produtos = supabase.table("produtos").select("*").execute().data
+produtos = supabase.schema("public").table("produtos").select("*").execute().data
 entradas = supabase.table("entradas_estoque").select("*").execute().data
 vendas = supabase.table("vendas").select("*").execute().data
 
